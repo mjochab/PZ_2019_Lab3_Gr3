0bdfe1a1e1e3edf084d23bf4671f34f02547497d
pawel2973 <pawel2973@gmail.com>
2019-04-08 22:22:38 +0200

    Merge fix

--- src/main/resources/db/migration/V1.1__InitRelations.sql
+++ src/main/resources/db/migration/V1.1__InitRelations.sql
@@ -12,10 +12,18 @@ add foreign key(user_id) references user(id),
 add foreign key(gift_id) references gift(id);
 
 alter table user
-add foreign key(role_id) references role(id);
+add foreign key(role_id) references role(id),
+add foreign key(adress_id) references adress(id);
 
 alter table user_order
 add foreign key(user_id) references user(id),
-add foreign key(courier_id) references courier(id);
+add foreign key(courier_id) references courier(id),
+add foreign key(sender_adress_id) references sender_adress(id),
+add foreign key(recipient_adress_id) references recipient_adress(id);
 
+alter table recipient_adress
+add foreign key(adress_id) references adress(id);
+
+alter table sender_adress
+add foreign key(adress_id) references adress(id);
 
