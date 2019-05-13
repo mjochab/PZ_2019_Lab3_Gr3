ddd8ce2c7153048b0020b28443cdc3ccc848eb3d
alan-bialy <alanbialyurz@gmail.com>
2019-05-13 16:23:00 +0200

    add displaying courier ratings, made courier rankings bigger

--- src/main/java/serwisPaczek/repository/OpinionRepository.java
+++ src/main/java/serwisPaczek/repository/OpinionRepository.java
@@ -2,8 +2,10 @@ package serwisPaczek.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import serwisPaczek.model.Courier;
 import serwisPaczek.model.Opinion;
 
 @Repository
 public interface OpinionRepository extends JpaRepository<Opinion, Long> {
+    Opinion findByUserOrder_Courier(Courier courier);
 }
