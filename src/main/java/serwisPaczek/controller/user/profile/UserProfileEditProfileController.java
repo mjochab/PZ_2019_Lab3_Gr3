effc199c5195d4263800581bcc6b587fd3e1f6c5
klaudia5 <37509846+klaudia5@users.noreply.github.com>
2019-05-14 18:25:27 +0200

    edit-user-profile

--- src/main/java/serwisPaczek/controller/user/profile/UserProfileEditProfileController.java
+++ src/main/java/serwisPaczek/controller/user/profile/UserProfileEditProfileController.java
@@ -5,11 +5,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import serwisPaczek.model.Adress;
+import serwisPaczek.model.User;
+import serwisPaczek.model.dto.UserAdressDto;
+import serwisPaczek.model.dto.UserLoginDto;
 import serwisPaczek.repository.AdressRepository;
 import serwisPaczek.utils.SceneManager;
 import serwisPaczek.utils.SceneType;
+import serwisPaczek.repository.UserRepository;
 
 import static serwisPaczek.model.dto.UserLoginDto.getLoggedUser;
+import static serwisPaczek.utils.DialogsUtils.showDialog;
 
 @Controller
 public class UserProfileEditProfileController {
@@ -17,6 +23,9 @@ public class UserProfileEditProfileController {
 
     @Autowired
     private AdressRepository adressRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @FXML
     private TextField TFphone;
@@ -45,6 +54,71 @@ public class UserProfileEditProfileController {
     @FXML
     public void BackToMenu(ActionEvent event) {
         sceneManager.show(SceneType.MAIN);
+    }
+
+    @FXML
+    public void EditUser(ActionEvent event) {
+
+        try {
+        if (TFname.getText().length() > 0
+                && TFsurname.getText().length() > 0
+                && TFcity.getText().length() > 0
+                && TFemail.getText().length() > 0
+                && TFzip.getText().length() > 0
+                && TFhome.getText().length() > 0
+                && this.TFhome.getText().matches("\\d+")
+                && TFphone.getText().length() > 0
+                && this.TFphone.getText().matches("\\d+")) {
+            String name = TFname.getText();
+            String surname = TFsurname.getText();
+            String city = TFcity.getText();
+            String email = TFemail.getText();
+            String zip = TFzip.getText();
+            String street = TFstreet.getText();
+            int home = Integer.parseInt(TFhome.getText());
+            long phone = Long.parseLong(TFphone.getText());
+
+            if (getLoggedUser().getAdress() != null) {
+
+                Adress adress = getLoggedUser().getAdress();
+                adress.setName(name);
+                adress.setSurname(surname);
+                adress.setStreet(street);
+                adress.setCity(city);
+                adress.setEmail(email);
+                adress.setZipCode(zip);
+                adress.setHouseNumber(home);
+                adress.setTelephoneNumber(phone);
+                adressRepository.save(adress);
+                User user = getLoggedUser();
+                user.setAdress(adress);
+                userRepository.save(user);
+                showDialog("Poprawna edycja profilu.");
+                sceneManager.show(SceneType.MAIN);
+
+            }
+
+            else {
+                Adress adress = new Adress(name, surname, city, street, home, zip, phone, email);
+                adressRepository.save(adress);
+                User user = getLoggedUser();
+                user.setAdress(adress);
+                userRepository.save(user);
+                showDialog("Poprawna edycja profilu.");
+                sceneManager.show(SceneType.MAIN);
+
+
+            }}
+
+
+         else {
+            throw new NumberFormatException();
+        }
+    } catch (NumberFormatException e) {
+            showDialog("Uzupełnij wszystkie pola poprawnymi danymi.");
+
+    }
+
     }
 
     @FXML
