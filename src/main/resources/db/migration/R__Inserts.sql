42bdac9fcc256144c2f52591374a742a7cd754af
alan-bialy <37514209+alan-bialy@users.noreply.github.com>
2019-05-21 09:26:44 +0200

    bugfixes -login errors

--- src/main/resources/db/migration/R__Inserts.sql
+++ src/main/resources/db/migration/R__Inserts.sql
@@ -2,6 +2,6 @@ insert into role(id,role_name) values(1,'ADMIN_ROLE');
 insert into role(id,role_name) values(2,'WORKER_ROLE');
 insert into role(id,role_name) values(3,'USER_ROLE');
 
-insert into user(id, username, password,role_id) values (1, 'admin', '$2a$10$yHxQpx7LOjWQWdN/Pu1h.uH7.9RoPiJB3nhR/zkQsr0Eiw.5hShEi',1);
-insert into user(id, username, password,role_id) values (2, 'worker', '$2a$10$U043P27lqXXsn9p6l0jRtuxlthVvV/u5HweK2vN8veJpPH.bSX7k.',2);
-insert into user(id, username, password,role_id) values (3, 'user', '$2a$10$9ZoqjxVrLLAiaYDtga1T/uicHAx5umJaxWt/H3G3NDPQQFHrJz/Fe',3);
+insert into user(id, username, password,role_id) values (1, 'admin123', '$2a$10$YrqwJz0w08j77FujYZxHIOy4z453Eb14ncQJV.dmSjcmR9QjLdvSy',1);
+insert into user(id, username, password,role_id) values (2, 'worker123', '$2a$10$RbZwTT4Eks0U2sZG7H79muRcP9v2ZgC0ovECB77Dy4Q3B9UT7lRfy.',2);
+insert into user(id, username, password,role_id) values (3, 'user123', '$2a$10$NYQcIjpEcK0ZggA6XW4L3u/wiQY9sRhl/QVwqj8.0oZ64nZ/WBi3e',3);
