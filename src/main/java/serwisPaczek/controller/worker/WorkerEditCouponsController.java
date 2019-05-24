cf840d609f357270c1e463195025757ad754a72a
k-duda <kkdudd@gmail.com>
2019-05-24 22:38:43 +0200

    Coupon validation

--- src/main/java/serwisPaczek/controller/worker/WorkerEditCouponsController.java
+++ src/main/java/serwisPaczek/controller/worker/WorkerEditCouponsController.java
@@ -4,11 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.Region;
 import javafx.util.converter.IntegerStringConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -55,6 +54,17 @@ public class WorkerEditCouponsController {
     public void addCoupon(ActionEvent event){
         Coupon coupon = new Coupon(addCouponName.getText(),Integer.valueOf(addDiscount.getText()));
         List<Coupon> couponList = couponRepository.findAll();
+        for (Coupon couponFindByName : couponList){
+            if (couponFindByName.getName().equals(coupon.getName())) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION,
+                        "Kupon o danej nazwie już istnieje", ButtonType.OK);
+                alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+                alert.setTitle("Komunikat");
+                alert.setHeaderText(null);
+                alert.show();
+                return;
+            }
+        }
         couponList.add(coupon);
         couponRepository.saveAll(couponList);
         fillTableView();
@@ -71,6 +81,18 @@ public class WorkerEditCouponsController {
     public void changeNameEvent(TableColumn.CellEditEvent event){
         Coupon coupon = tableView.getSelectionModel().getSelectedItem();
         coupon.setName(event.getNewValue().toString());
+        List<Coupon> couponList = couponRepository.findAll();
+        for (Coupon couponFindByName : couponList){
+            if (couponFindByName.getName().equals(coupon.getName())) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION,
+                        "Kupon o danej nazwie już istnieje", ButtonType.OK);
+                alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+                alert.setTitle("Komunikat");
+                alert.setHeaderText(null);
+                alert.show();
+                return;
+            }
+        }
         couponRepository.save(coupon);
         fillTableView();
     }
